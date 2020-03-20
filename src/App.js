749ad5dab8611ced1fc5c8ec639c9e1cba29f7a0
Michael Harms <michael.harms6010@gmail.com>
2020-03-20 17:58:55 -0500

    cra, implements sass, adds npm lock to gitignore, css reset

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A word find app goes here</h1>
+    </div>
+  );
+}
+
+export default App;
