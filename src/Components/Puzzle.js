73a1143a79b98dd20ff0dcaf5013cb2d2007919f
Kristi Gribble <kgribble4083@gmail.com>
2020-03-25 22:16:49 -0500

    Removed some Puzzle code - will finish it tomorrow maybe

--- src/Components/Puzzle.js
+++ src/Components/Puzzle.js
@@ -1,19 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 
 const Puzzle = () => {
 
-    useEffect(() => {
-        axios
-            .get('/wordlists')
-            .then(response => {
-                console.log(response.data)
-            })
-            .catch(err => {
-                console.log("Error getting the Puzzle")
-            })
-    })
     return (
         <div>We're displaying a puzzle here!
         
