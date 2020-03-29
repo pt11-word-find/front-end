ba5bfdcddded421f246a80e4db28184cc2a20d14
Kristi Gribble <kgribble4083@gmail.com>
2020-03-28 21:17:32 -0500

    Added code to color selected letters and check if it matches the wordlist

--- src/Components/Puzzle.js
+++ src/Components/Puzzle.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import wordSearch from "../utils/wordSearch-generator";
+import arrayIncluded from "../utils/arrayIncluded";
+import arrayEqual from "../utils/arrayEqual";
 
 const Puzzle = () => {
     
@@ -18,12 +20,32 @@ const Puzzle = () => {
         setPuzzle(wordSearch(wordlist.map(item => item.toUpperCase()), 10, 10, 1))
     }, [wordlist])
 
-    // useEffect(() => {
-    //     console.log(puzzle)
-    // }, [puzzle])
+    useEffect(() => {
+        let sortedSelect = selectLetter.map(item => puzzle[item[0]][item[1]]).sort()
+        
+        wordlist.map(item => {
+            if (arrayEqual(sortedSelect, item.toUpperCase().split("").sort())) {
+                console.log(item.split("").sort())
+                console.log("Match")
+            }
+            
+        })
+    }, [selectLetter])
 
-    const handleChange = event => {
-        setSelectLetter({ ...selectLetter, [event.target.name]: event.target.value })
+
+    // tile = [row, col]
+    // selectLetter is an array
+    // setSelectLetter is a function that sets selectLetter
+    // if selectLetter contains tile, remove tile
+    // if selectLetter does not contain tile, add tile to the array, color letter to indicate it's in the array.
+    
+    const toggleLetter = (r_index, c_index) => {
+        if (arrayIncluded(selectLetter, [r_index, c_index])) {
+            setSelectLetter(selectLetter.filter(item => !arrayEqual(item, [r_index, c_index])))
+        } else {
+            setSelectLetter([...selectLetter, [r_index, c_index]])
+            console.log("select letter", selectLetter)
+        }
     }
 
     return (
@@ -32,7 +54,7 @@ const Puzzle = () => {
             {puzzle.map((row, r_index) => 
                 <div key={r_index + row} className="row">
                     {row.map((tile, c_index) => 
-                        <div key={c_index + tile} className="tile" id="tile">
+                        <div key={c_index + tile} className={arrayIncluded(selectLetter, [r_index, c_index]) ? "selectedTile tile" : "tile"} onClick={() => toggleLetter(r_index, c_index)}>
                             {tile}
                         </div>
                     )}
