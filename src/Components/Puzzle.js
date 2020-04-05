9e3c0c202c84518965f045a13faae8bf4c7c65d5
Kristi Gribble <kgribble4083@gmail.com>
2020-04-05 13:52:02 -0500

    Fixed font button and crossed out functionality

--- src/Components/Puzzle.js
+++ src/Components/Puzzle.js
@@ -124,7 +124,7 @@ const Puzzle = (props) => {
                     )}
                 </div>
             )}
-            {wordlist.length > 0 ? <button style={{width: "100px", margin: "1% auto" }} className="manage-button" onClick={_ => setFont( (font + 1) % 5 )}>Toggle font</button> : null }
+            {wordlist.length > 0 ? <button className="manage-button" onClick={_ => setFont( (font + 1) % 5 )}>Toggle font</button> : null }
         </div>
         <div className="word-list">
             {wordlist.map(word => 
