2730be7e8355948ca548e07b88ce1a7e3dbcb78f
Kristi Gribble <kgribble4083@gmail.com>
2020-04-04 23:05:53 -0500

    added delete to personal puzzles, manage personal puzzles

--- src/Components/PuzzleList.js
+++ src/Components/PuzzleList.js
@@ -3,6 +3,7 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 import "../App.scss"
 import WordContext from "../contexts/WordContext"
 import { Link } from "react-router-dom";
+import ManagePuzzles from "./ManagePuzzles";
 
 const PuzzleList=()=>{
     const { puzzles, setPuzzles }= useContext(WordContext);
@@ -11,23 +12,25 @@ const PuzzleList=()=>{
       axiosWithAuth()
         .get("/wordlists")
         .then (response =>{
+          console.log(response)
         setPuzzles(response.data)
         })
     }, [])
 
 
-
-  return(
-    <>
-    <h3>Choose a puzzle:</h3>
-
-    {puzzles.length > 0 ? <div className="puzzle-list">
-    {puzzles.map(item => {
-      return <Link to={`/puzzles/${item.id}`}><div className="puzzle-link">{item.title}</div></Link>})}   
-    </div> 
-    : <h3>Loading ...</h3>}
-    </>
-  )
-
-}
+    return(
+      <>
+      <h3>Choose a puzzle:</h3>
+    
+      <Link to={`/managepuzzles`}>Manage My Puzzles</Link>
+    
+      {puzzles.length > 0 ? <div className="puzzle-list">
+      {puzzles.map(item => {
+        return <div className="puzzle-link"><Link to={`/puzzles/${item.id}`}>{item.title}</Link></div>})}   
+      </div> 
+      : <h3>Loading ...</h3>}
+      </>
+    )
+  
+  }
 export default PuzzleList;
